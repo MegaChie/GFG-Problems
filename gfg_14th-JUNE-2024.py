1c3a47f5a6e623901e281a4c4b6f3390220c77c2
MegaChie <workspace.mega@gmail.com>
2024-06-14 05:33:20 -0700

    Fix logic

--- gfg_14th-JUNE-2024.py
+++ gfg_14th-JUNE-2024.py
@@ -4,7 +4,7 @@
 def armstrongNumber (n):
     """Returns Yes if passed number is an Armstrong number, No if it isn't"""
     digits = [int(x) for x in str(n)]
-    digits = [x ** 3 for x in digits]
+    digits = [x ** len(digits) for x in digits]
     if sum(digits) == n:
         return "Yes"
     else:
